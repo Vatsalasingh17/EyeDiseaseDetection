e994458cae273fac7e2b63ff76b06ec7de286399
Vatsala Singh <singhvatsala2004@gmail.com>
2025-06-01 23:55:11 +0530

    Update utils.py

--- api/utils.py
+++ api/utils.py
@@ -1,1 +1,11 @@
+from PIL import Image
+import numpy as np
+import cv2
+
+def preprocess_image(image_path, size=224):
+    image = cv2.imread(image_path)
+    image = cv2.resize(image, (size, size))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image / 255.0  # normalize
+    return np.expand_dims(image, axis=0)
 
